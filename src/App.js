23eb16ebeb5f189420d81e6251e45b8194328977
Sarath <saraththekkadavan@gmail.com>
2023-11-28 02:14:42 +0530

    code allign

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-
-import { useState } from 'react';
-import './App.css';
-import Details from './Components/Details';
-import List from './Components/List';
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import axios from 'axios';
+import { useState } from "react";
+import "./App.css";
+import Details from "./Components/Details";
+import List from "./Components/List";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
-  const [Data, setdata] = useState([])
-  axios.get('https://jsonplaceholder.typicode.com/users').then((res)=>{
-    console.log(res.data);
-    setdata(res.data)
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+  const [Data, setdata] = useState([]);
+  axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      console.log(res.data);
+      setdata(res.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 
   return (
     <div className="App">
-   
       <Router>
-
         <Routes>
-          <Route path='/' element={<List dat={Data}/>}/>
-          <Route path='/details/:id' element={<Details  dat={Data}/>}/>
-         
+          <Route path="/" element={<List dat={Data} />} />
+          <Route path="/details/:id" element={<Details dat={Data} />} />
         </Routes>
-
-        </Router>
-      
+      </Router>
     </div>
   );
 }
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
+import React from "react";
 import { BsArrowRightCircle } from "react-icons/bs";
-import './List.css';
-import { useNavigate } from 'react-router-dom';
+import "./List.css";
+import { useNavigate } from "react-router-dom";
 
+function List({ dat }) {
+  const nav = useNavigate();
 
-
-function List({dat}) {
-     const nav = useNavigate()
-
-    const Display =(id)=>{
-       nav(`/details/${id}`)
-    }
+  const Display = (id) => {
+    nav(`/details/${id}`);
+  };
   return (
-    <div className='container'>
-        <div className='main'>
-            <h1 className='header'>User Details</h1>
-        </div>
-        <div className='border'>
-            {dat.map((res)=>{
-                return(
-                    <div key={res.id} className='details'>
-                    <ul>
-                        <li className='list'><h5>{res.name}</h5></li>
-                        <li className='list'>{res.email}</li>
-                        <li className='list'>{res.phone}</li>
-                    </ul>
-                    <button onClick={()=>Display(res.id)} className='icon'> <BsArrowRightCircle/> 
-        </button>
-                </div>
-
-                )
-            })}
-       
-        </div>
-       
+    <div className="container">
+      <div className="main">
+        <h1 className="header">User Details</h1>
+      </div>
+      <div className="border">
+        {dat.map((res) => {
+          return (
+            <div key={res.id} className="details">
+              <ul>
+                <li className="list">
+                  <h5>{res.name}</h5>
+                </li>
+                <li className="list">{res.email}</li>
+                <li className="list">{res.phone}</li>
+              </ul>
+              <button onClick={() => Display(res.id)} className="icon">
+                {" "}
+                <BsArrowRightCircle />
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default List
-
+export default List;
